1f984cc72d54180107f8bb405c1d1ca281e65bed
philip.laureano <philip.laureano@09580e82-21c1-11de-b56b-c9554f25afe1>
2009-04-20 09:02:51 +0000

    git-svn-id: http://hiro.googlecode.com/svn/trunk@22 09580e82-21c1-11de-b56b-c9554f25afe1

---
+++ src/Core/Implementations/ConstructorImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Hiro.Interfaces;
+
+namespace Hiro.Implementations
+{
+    public class ConstructorImplementation : Implementation<ConstructorInfo>
+    {
+        public ConstructorImplementation(ConstructorInfo constructor, IDependencyResolver<ConstructorInfo> resolver)
+            : base(constructor, resolver)
+        {
+        }
+    }
+}
---
+++ src/Core/Implementations/PropertyImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Hiro.Interfaces;
+
+namespace Hiro.Implementations
+{
+    public class PropertyImplementation : Implementation<PropertyInfo>
+    {
+        public PropertyImplementation(PropertyInfo property, IDependencyResolver<PropertyInfo> resolver)
+            : base(property, resolver)
+        {
+        }
+    }
+}
