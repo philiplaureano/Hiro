75d30f2fc448f0a49409790975756da97b3edb70
philip.laureano <philip.laureano@09580e82-21c1-11de-b56b-c9554f25afe1>
2009-05-14 22:05:32 +0000

    git-svn-id: http://hiro.googlecode.com/svn/trunk@47 09580e82-21c1-11de-b56b-c9554f25afe1

---
+++ src/Core/Implementations/PropertyInjector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hiro.Interfaces;
+
+namespace Hiro.Implementations
+{
+    /// <summary>
+    /// Represents a class that adds property injection calls to an existing <see cref="IImplementation"/> instance.
+    /// </summary>
+	public class PropertyInjector : IImplementationInjector
+	{
+        /// <summary>
+        /// Injects the target <paramref name="IImplementation"/> instance.
+        /// </summary>
+        /// <param name="dependency">The target dependency.</param>
+        /// <param name="originalImplementation">The target implementation that will be intercepted by this method.</param>
+        /// <returns>The <see cref="IImplementation"/> instance that will be injected in place of the original implementation.</returns>
+        public IImplementation Inject(IDependency dependency, IImplementation originalImplementation)
+        {
+            return new PropertyInjectionCall(originalImplementation);
+        }
+    }
+}
