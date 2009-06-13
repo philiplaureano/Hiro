fd29e7d280c4637ca6749abedbed03b2aa2bd396
philip.laureano <philip.laureano@09580e82-21c1-11de-b56b-c9554f25afe1>
2009-06-13 05:42:14 +0000

    Changes:
    -Moved the basic behavior of the DependencyMap class into BaseDependencyMap class
    -Moved the DependencyMapExtensions into the DependencyMap class
    
    git-svn-id: http://hiro.googlecode.com/svn/trunk@49 09580e82-21c1-11de-b56b-c9554f25afe1

---
+++ src/Core/BaseDependencyMap.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hiro.Interfaces;
+using NGenerics.DataStructures.General;
+
+namespace Hiro
+{
+    /// <summary>
+    /// Represents a class that can map dependencies to implementations.
+    /// </summary>
+    public class BaseDependencyMap : IDependencyMap
+    {
+        /// <summary>
+        /// The list of dependencies in the current map.
+        /// </summary>
+        private HashList<IDependency, IImplementation> _entries = new HashList<IDependency, IImplementation>();
+
+        /// <summary>
+        /// Gets or sets the value indicating the <see cref="IImplementationInjector"/> instance that will be used to intercept <see cref="IImplementation"/> instances.
+        /// </summary>
+        /// <value>The implementation injector.</value>
+        public IImplementationInjector Injector { get; set; }
+
+        /// <summary>
+        /// Gets the value indicating the list of dependencies that currently exist within the current container.
+        /// </summary>
+        /// <value>The current list of dependencies.</value>
+        public IEnumerable<IDependency> Dependencies
+        {
+            get
+            {
+                return _entries.Keys;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether or not a particular service dependency exists in the current dependency container.
+        /// </summary>
+        /// <param name="dependency">The target service dependency.</param>
+        /// <returns><c>true</c> if the service exists; otherwise, it will return <c>false</c>.</returns>
+        public bool Contains(IDependency dependency)
+        {
+            return _entries.ContainsKey(dependency);
+        }
+
+        /// <summary>
+        /// Associates the given <paramref name="implementation"/> with the target <paramref name="dependency"/>.
+        /// </summary>
+        /// <param name="dependency">The dependency that will be associated with the implementation.</param>
+        /// <param name="implementation">The implementation itself.</param>
+        public void AddService(IDependency dependency, IImplementation implementation)
+        {
+            var currentImplementation = implementation;
+
+            if (Injector != null)
+                currentImplementation = Injector.Inject(dependency, currentImplementation);
+
+            _entries.Add(dependency, currentImplementation);
+        }
+
+        /// <summary>
+        /// Gets the current list of implementations for the current dependency.
+        /// </summary>
+        /// <param name="targetDependency">The target dependency.</param>
+        /// <param name="addIncompleteImplementations">A boolean flag that determines whether or not the resulting list should include implementations with incomplete dependencies.</param>
+        /// <returns>A list of implementations.</returns>
+        public IEnumerable<IImplementation> GetImplementations(IDependency targetDependency, bool addIncompleteImplementations)
+        {
+            if (!_entries.ContainsKey(targetDependency))
+                yield break;
+
+            var items = _entries[targetDependency];
+            foreach (var item in items)
+            {
+                var missingDependencies = item.GetMissingDependencies(this);
+                var missingItems = new List<IDependency>(missingDependencies);
+                var completed = missingItems.Count == 0;
+
+                if (completed || (!completed && addIncompleteImplementations))
+                    yield return item;
+            }
+        }
+    }
+}
--- src/Core/DependencyMap.cs
+++ src/Core/DependencyMap.cs
@@ -1,85 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using Hiro.Interfaces;
-using NGenerics.DataStructures.General;
+using Hiro.Containers;
+using Hiro.Implementations;
+using LinFu.Reflection.Emit;
 
 namespace Hiro
 {
     /// <summary>
     /// Represents a class that can map dependencies to implementations.
     /// </summary>
-    public class DependencyMap : IDependencyMap
+    public class DependencyMap : BaseDependencyMap
     {
         /// <summary>
-        /// The list of dependencies in the current map.
+        /// Adds a service to the dependency map.
         /// </summary>
-        private HashList<IDependency, IImplementation> _entries = new HashList<IDependency, IImplementation>();
-
-        /// <summary>
-        /// Gets or sets the value indicating the <see cref="IImplementationInjector"/> instance that will be used to intercept <see cref="IImplementation"/> instances.
-        /// </summary>
-        /// <value>The implementation injector.</value>
-        public IImplementationInjector Injector { get; set; }
-
-        /// <summary>
-        /// Gets the value indicating the list of dependencies that currently exist within the current container.
-        /// </summary>
-        /// <value>The current list of dependencies.</value>
-        public IEnumerable<IDependency> Dependencies
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementation">The concrete type that will implement the service type.</typeparam>
+        public void AddService<TService, TImplementation>()
+            where TImplementation : TService
         {
-            get
-            {
-                return _entries.Keys;
-            }
+            AddService(typeof(TService), typeof(TImplementation));
         }
 
         /// <summary>
-        /// Determines whether or not a particular service dependency exists in the current dependency container.
+        /// Adds a service to the dependency map.
         /// </summary>
-        /// <param name="dependency">The target service dependency.</param>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementation">The concrete type that will implement the service type.</typeparam>
+        /// <param name="serviceName">The service name.</param>
+        public void AddService<TService, TImplementation>(string serviceName)
+            where TImplementation : TService
+        {
+            AddService(serviceName, typeof(TService), typeof(TImplementation));
+        }
+
+        /// <summary>
+        /// Adds a service to the dependency map.
+        /// </summary>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="implementingType">The implementing type.</param>
+        public void AddService(string serviceName, Type serviceType, Type implementingType)
+        {
+            if (!serviceType.IsAssignableFrom(implementingType))
+                throw new ArgumentException("The implementing type must be derived from the service type");
+
+            AddService(new Dependency(serviceType, serviceName), new TransientType(implementingType, this));
+        }
+
+        /// <summary>
+        /// Adds a service to the dependency map.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="implementingType">The implementing type.</param>
+        public void AddService(Type serviceType, Type implementingType)
+        {
+            if (!serviceType.IsAssignableFrom(implementingType))
+                throw new ArgumentException("The implementing type must be derived from the service type");
+
+            AddService(new Dependency(serviceType), new TransientType(implementingType, this));
+        }
+
+        /// <summary>
+        /// Adds a service to the dependency map.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementation">The concrete type that will implement the service type.</typeparam>
+        public void AddSingletonService<TService, TImplementation>()
+            where TImplementation : TService
+        {
+            AddService(new Dependency(typeof(TService)), new SingletonType(typeof(TImplementation), this));
+        }
+
+        /// <summary>
+        /// Adds a service to the dependency map.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementation">The concrete type that will implement the service type.</typeparam>
+        /// <param name="serviceName">The service name.</param>
+        public void AddSingletonService<TService, TImplementation>(string serviceName)
+        {
+            AddService(new Dependency(typeof(TService), serviceName), new SingletonType(typeof(TImplementation), this));
+        }
+
+        /// <summary>
+        /// Adds a singleton service to the dependency map.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="implementingType">The implementing type.</param>
+        public void AddSingletonService(Type serviceType, Type implementingType)
+        {
+            if (!serviceType.IsAssignableFrom(implementingType))
+                throw new ArgumentException("The implementing type must be derived from the service type");
+
+            AddService(new Dependency(serviceType), new SingletonType(implementingType, this));
+        }
+
+        /// <summary>
+        /// Adds a singleton service to the dependency map.
+        /// </summary>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="implementingType">The implementing type.</param>
+        public void AddSingletonService(string serviceName, Type serviceType, Type implementingType)
+        {
+            if (!serviceType.IsAssignableFrom(implementingType))
+                throw new ArgumentException("The implementing type must be derived from the service type");
+
+            AddService(new Dependency(serviceType, serviceName), new SingletonType(implementingType, this));
+        }
+
+        /// <summary>
+        /// Determines whether or not a service exists within the dependency map.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="serviceName">The service name.</param>
         /// <returns><c>true</c> if the service exists; otherwise, it will return <c>false</c>.</returns>
-        public bool Contains(IDependency dependency)
+        public bool Contains(Type serviceType, string serviceName)
         {
-            return _entries.ContainsKey(dependency);
+            return Contains(new Dependency(serviceType, serviceName));
         }
 
         /// <summary>
-        /// Associates the given <paramref name="implementation"/> with the target <paramref name="dependency"/>.
+        /// Determines whether or not a service exists within the dependency map.
         /// </summary>
-        /// <param name="dependency">The dependency that will be associated with the implementation.</param>
-        /// <param name="implementation">The implementation itself.</param>
-        public void AddService(IDependency dependency, IImplementation implementation)
+        /// <param name="map">The dependency map.</param>
+        /// <param name="serviceType">The service type.</param>
+        /// <returns><c>true</c> if the service exists; otherwise, it will return <c>false</c>.</returns>
+        public bool Contains(Type serviceType)
         {
-            var currentImplementation = implementation;
-
-            if (Injector != null)
-                currentImplementation = Injector.Inject(dependency, currentImplementation);
-
-            _entries.Add(dependency, currentImplementation);
+            return Contains(new Dependency(serviceType));
         }
 
         /// <summary>
-        /// Gets the current list of implementations for the current dependency.
+        /// Compiles and instantiates a container instance using the current dependencies in the dependency map.
         /// </summary>
-        /// <param name="targetDependency">The target dependency.</param>
-        /// <param name="addIncompleteImplementations">A boolean flag that determines whether or not the resulting list should include implementations with incomplete dependencies.</param>
-        /// <returns>A list of implementations.</returns>
-        public IEnumerable<IImplementation> GetImplementations(IDependency targetDependency, bool addIncompleteImplementations)
+        /// <param name="map">The dependency map.</param>
+        /// <returns>A <see cref="IMicroContainer"/> instance.</returns>
+        public IMicroContainer CreateContainer()
         {
-            if (!_entries.ContainsKey(targetDependency))
-                yield break;
+            var compiler = new ContainerCompiler();
+            var assembly = compiler.Compile(this);
+            var loadedAssembly = assembly.ToAssembly();
 
-            var items = _entries[targetDependency];
-            foreach (var item in items)
+            var containerTypes = new List<Type>();
+            foreach (var type in loadedAssembly.GetTypes())
             {
-                var missingDependencies = item.GetMissingDependencies(this);
-                var missingItems = new List<IDependency>(missingDependencies);
-                var completed = missingItems.Count == 0;
+                if (!typeof(IMicroContainer).IsAssignableFrom(type))
+                    continue;
 
-                if (completed || (!completed && addIncompleteImplementations))
-                    yield return item;
+                containerTypes.Add(type);
             }
-        }        
+
+            var containerType = containerTypes[0];
+            IMicroContainer result = (IMicroContainer)Activator.CreateInstance(containerType);
+
+            return result;
+        }
     }
 }
