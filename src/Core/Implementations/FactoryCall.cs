cc53f54c8dd9d3a8546179fbde7255d20a59dedb
philip.laureano <philip.laureano@09580e82-21c1-11de-b56b-c9554f25afe1>
2010-03-28 09:48:32 +0000

    Added the missing XML doc comments for the FactoryCall class and added the missing references to Hiro.Containers.dll in the Hiro.build file
    git-svn-id: http://hiro.googlecode.com/svn/trunk@83 09580e82-21c1-11de-b56b-c9554f25afe1

--- src/Core/Implementations/FactoryCall.cs
+++ src/Core/Implementations/FactoryCall.cs
@@ -8,27 +8,50 @@ using Mono.Cecil.Cil;
 
 namespace Hiro.Implementations
 {
+    /// <summary>
+    /// Represents a class that emits a call to an <see cref="IFactory{T}"/> instance to instantiate a particular service instance.
+    /// </summary>
     public class FactoryCall : IImplementation
     {
         private readonly Type _serviceType;
         private readonly string _serviceName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FactoryCall"/> class.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="serviceName">The service name.</param>
         public FactoryCall(Type serviceType, string serviceName)
         {
             _serviceType = serviceType;
             _serviceName = serviceName;
         }
 
+        /// <summary>
+        /// Gets the list of missing dependencies from the current implementation.
+        /// </summary>
+        /// <param name="map">The implementation map.</param>
+        /// <returns>A list of missing dependencies.</returns>
         public IEnumerable<IDependency> GetMissingDependencies(IDependencyContainer map)
         {
             yield break;
         }
 
+        /// <summary>
+        /// Returns the dependencies required by the current implementation.
+        /// </summary>
+        /// <returns>The list of required dependencies required by the current implementation.</returns>
         public IEnumerable<IDependency> GetRequiredDependencies()
         {
             yield break;
         }
 
+        /// <summary>
+        /// Emits the <see cref="IFactory{T}.Create"/> method call that will instantiate the current service instance.
+        /// </summary>
+        /// <param name="dependency">The dependency that describes the service to be instantiated.</param>
+        /// <param name="serviceMap">The service map that contains the list of dependencies in the application.</param>
+        /// <param name="targetMethod">The target method.</param>
         public void Emit(IDependency dependency, IDictionary<IDependency, IImplementation> serviceMap, MethodDefinition targetMethod)
         {
             var factoryType = typeof (IFactory<>).MakeGenericType(_serviceType);
