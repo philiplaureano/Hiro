7970fbbef7da3e88cd840c6f95f0b62bc5a31583
Philip Laureano <philip.laureano@gmail.com>
2011-03-20 14:42:21 +0800

    Fixed ILMerge errors in the release build and added the missing files for the functor tests

---
+++ src/UnitTests/SampleModel/Foo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hiro.Functors.UnitTests.SampleModel
+{
+    public class Foo : IFoo
+    {
+    }
+}
